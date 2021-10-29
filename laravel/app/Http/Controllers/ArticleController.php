f295d07227b9cd168d4e0be38d2438e65c75ddbc
ryoichi-k <kazyuapple99@gmail.com>
2021-10-29 12:57:23 +0900

    first

---
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        // ダミーデータ
+        $articles = [
+            (object) [
+                'id' => 1,
+                'title' => 'タイトル1',
+                'body' => '本文1',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 1,
+                    'name' => 'ユーザー名1',
+                ],
+            ],
+            (object) [
+                'id' => 2,
+                'title' => 'タイトル2',
+                'body' => '本文2',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 2,
+                    'name' => 'ユーザー名2',
+                ],
+            ],
+            (object) [
+                'id' => 3,
+                'title' => 'タイトル3',
+                'body' => '本文3',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 3,
+                    'name' => 'ユーザー名3',
+                ],
+            ],
+            (object) [
+                'id' => 4,
+                'title' => 'ドナルド',
+                'body' => 'うわさ',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 4,
+                    'name' => 'ドナルド・マクドナルド',
+                ],
+            ],
+        ];
+
+        return view('articles.index', ['articles' => $articles]);
+    }
+}
