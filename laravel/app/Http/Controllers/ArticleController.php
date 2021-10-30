4d63db7aebebe8a50f40b3d745337efbbe76a227
ryoichi-k <kazyuapple99@gmail.com>
2021-10-30 17:44:45 +0900

    fix:ArticleController.php

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -17,5 +18,13 @@ class ArticleController extends Controller
     public function create()
     {
         return view('articles.create');
+    }
+
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all());
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
     }
 }
