8f3e07182d642717b8f7dfe3d312843d1b51022f
ryoichi-k <kazyuapple99@gmail.com>
2021-11-05 21:42:23 +0900

    add:follow

---
+++ laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function show(string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        $articles = $user->articles->sortByDesc('created_at');
+
+        return view('users.show', [
+            'user' => $user,
+            'articles' => $articles,
+        ]);
+    }
+
+    public function likes(string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        $articles = $user->likes->sortByDesc('created_at');
+
+        return view('users.likes', [
+            'user' => $user,
+            'articles' => $articles,
+        ]);
+    }
+
+    public function follow(Request $request, string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        if ($user->id === $request->user()->id)
+        {
+            return abort('404', 'Cannot follow yourself.');
+        }
+
+        $request->user()->followings()->detach($user);
+        $request->user()->followings()->attach($user);
+
+        return ['name' => $name];
+    }
+
+    public function unfollow(Request $request, string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        if ($user->id === $request->user()->id)
+        {
+            return abort('404', 'Cannot follow yourself.');
+        }
+
+        $request->user()->followings()->detach($user);
+
+        return ['name' => $name];
+    }
+}
