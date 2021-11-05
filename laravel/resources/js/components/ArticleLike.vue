ec20027b47fe72923be1cf64d3680984cba109c6
ryoichi-k <kazyuapple99@gmail.com>
2021-11-05 10:52:17 +0900

    add:iine

--- laravel/resources/js/components/ArticleLike.vue
+++ laravel/resources/js/components/ArticleLike.vue
@@ -5,10 +5,11 @@
       class="btn m-0 p-1 shadow-none"
     >
       <i class="fas fa-heart mr-1"
-        :class="{'red-text':this.isLikedBy}"
+        :class="{'red-text':this.isLikedBy, 'animated heartBeat fast':this.gotToLike}"
+        @click="clickLike"
       />
     </button>
-    {{ countLikes }} 
+    {{ countLikes }}
   </div>
 </template>
 
@@ -23,12 +24,47 @@
         type: Number,
         default: 0,
       },
+      authorized: {
+        type: Boolean,
+        default: false,
+      },
+      endpoint: {
+        type: String,
+      },
     },
     data() {
       return {
         isLikedBy: this.initialIsLikedBy,
         countLikes: this.initialCountLikes,
+        gotToLike: false,
       }
     },
+    methods: {
+      clickLike() {
+        if (!this.authorized) {
+          alert('いいね機能はログイン中のみ使用できます')
+          return
+        }
+
+        this.isLikedBy
+          ? this.unlike()
+          : this.like()
+      },
+      async like() {
+        const response = await axios.put(this.endpoint)
+
+        this.isLikedBy = true
+        this.countLikes = response.data.countLikes
+        this.gotToLike = true
+      },
+      async unlike() {
+        const response = await axios.delete(this.endpoint)
+
+        this.isLikedBy = false
+        this.countLikes = response.data.countLikes
+        this.gotToLike = false
+      },
+    },
+
   }
 </script>
